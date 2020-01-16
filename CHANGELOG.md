9fe94667af9d3015a2df485f0bbbbabdffb573d4
James Alexander <j.alex@email.com>
2020-01-16 12:30:34 -0800

    Formatting changes.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.1.0+1] - January 16, 2020
+
+* Formatting changes.
+
 ## [0.1.0] - July 17, 2019
 
-* Initial release
+* Initial release.
--- test/broadcast_stream_subscriber_test.dart
+++ test/broadcast_stream_subscriber_test.dart
@@ -7,12 +7,12 @@ void main() {
 
   String _eventTest;
 
-  test('BroadcastStreamSubscriber mixes in methods to manage streams on a class.', () async {
+  test('Listen and Close Stream', () async {
     // Initialize a class to test the mixed in methods.
-    final _BroadcastStreamSubscriberTest test = _BroadcastStreamSubscriberTest();
+    final test = _BroadcastStreamSubscriberTest();
 
     // Add a listener and do something with the event data
-    test.addListener((String event) => _eventTest = event);
+    test.addListener((event) => _eventTest = event);
 
     // Confirm the listener was added
     expect(test.updateNotifier.hasListener, equals(true));
@@ -23,7 +23,8 @@ void main() {
     // Wait 50ms so the listener can be notified
     await Future.delayed(const Duration(milliseconds: 50), (){});
 
-    // Confirm the listener was notified by checking the variable set by it's event data
+    // Confirm the listener was notified by checking
+    // the variable set by it's event data
     expect(_eventTest, equals(_testString));
 
     // Remove the listener and close the stream
